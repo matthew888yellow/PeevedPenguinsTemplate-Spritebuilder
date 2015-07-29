789a9a6b1a13d68767e50c286befa094436d7e90
matthew888yellow <Matthew@Matthews-MacBook-Pro.local>
2015-07-28 20:54:03 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Matthew Huang on 7/28/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
